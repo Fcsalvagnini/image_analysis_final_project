1b3064de5af50f16c9f4873f7a69a899de1f781a
Fcsalvagnini <felip.eng@outlook.com.br>
2022-05-06 21:54:08 -0300

    Fix segmentation fault when passing a pointer to a pointer to an iftSet in the MyDilateBin method

--- project01.c
+++ project01.c
@@ -1,7 +1,7 @@
 #include "ift.h"
 
-/* 
-   Project 01, course MO445, Prof. Alexandre Falcao. 
+/*
+   Project 01, course MO445, Prof. Alexandre Falcao.
 
    This code crops a region of interest containing a fingerprint with
    as minimum as possible surrounding noise.
@@ -23,10 +23,10 @@
 
 /* it returns pixels at the border of the image */
 
-iftSet *MyImageBorder(iftImage *bin)
-{
+// iftSet *MyImageBorder(iftImage *bin)
+// {
 
-}
+// }
 
 /* it returns a set with internal border pixels */
 
@@ -88,146 +88,158 @@ iftSet *MyBackgroundBorder(iftImage *bin)
 
 iftImage *MyDilateBin(iftImage *bin, iftSet **S, float radius)
 {
+   /* Creates cost matrix with all values as 0, and then
+   set then all to INFINITY_INT*/
+   iftImage *cost = iftCreateImageFromImage(bin);
+   iftSetImage(cost, IFT_INFINITY_INT);
+   iftImage *dilated_bin = iftCopyImage(bin);
+   printf("[0] Set size => %d\n", iftSetSize(*S));
+   *S = MyObjectBorder(bin);
+   printf("[1] Set size => %d\n", iftSetSize(*S));
 
-
-
+   return dilated_bin;
 }
 
 /* it erodes objects */
 
-iftImage *MyErodeBin(iftImage *bin, iftSet **S, float radius)
-{
+// iftImage *MyErodeBin(iftImage *bin, iftSet **S, float radius)
+// {
 
 
 
-}
+// }
 
 /* it executes dilation followed by erosion */
 
-iftImage *MyCloseBin(iftImage *bin, float radius)
-{
+// iftImage *MyCloseBin(iftImage *bin, float radius)
+// {
 
 
 
-}
+// }
 
 /* it executes erosion followed by dilation */
 
-iftImage *MyOpenBin(iftImage *bin, float radius)
-{
+// iftImage *MyOpenBin(iftImage *bin, float radius)
+// {
 
 
 
-}
+// }
 
 /* it executes closing followed by opening */
 
-iftImage *MyAsfCOBin(iftImage *bin, float radius)
-{
- 
+// iftImage *MyAsfCOBin(iftImage *bin, float radius)
+// {
 
 
-}
+
+// }
 
 /* it closes holes in objects */
 
-iftImage *MyCloseBasins(iftImage *bin)
-{
+// iftImage *MyCloseBasins(iftImage *bin)
+// {
 
 
-  
-}
+
+// }
 
 int main(int argc, char *argv[])
 {
-  timer *tstart=NULL;
-  char   filename[200];
-  
-  /*--------------------------------------------------------*/
+   timer *tstart=NULL;
+   char   filename[200];
 
-  void *trash = malloc(1);                 
-  struct mallinfo info;   
-  int MemDinInicial, MemDinFinal;
-  free(trash); 
-  info = mallinfo();
-  MemDinInicial = info.uordblks;
+   /*--------------------------------------------------------*/
+   void *trash = malloc(1);
+   struct mallinfo info;
+   int MemDinInicial, MemDinFinal;
+   free(trash);
+   info = mallinfo();
+   MemDinInicial = info.uordblks;
+   /*--------------------------------------------------------*/
 
-  /*--------------------------------------------------------*/
 
-  
-  if (argc != 3) {
-    printf("project01 <P1> <P2>\n");
-    printf("P1: folder with original images\n");
-    printf("P2: folder with cropped images\n");
-    exit(0);
-  }
+   if (argc != 3) {
+      printf("project01 <P1> <P2>\n");
+      printf("P1: folder with original images\n");
+      printf("P2: folder with cropped images\n");
+      exit(0);
+   }
 
-  tstart = iftTic();
+   tstart = iftTic();
 
-  iftFileSet *fs   = iftLoadFileSetFromDirBySuffix(argv[1],".png", 1);  
-  int nimages      = fs->n;
-  char *out_dir    = argv[2];
-  iftMakeDir(out_dir);
-  iftAdjRel *A     = iftCircular(3.5), *B = iftCircular(1.5);
-  
-  for (int i=0; i < nimages; i++) {
-    char *basename = iftFilename(fs->files[i]->path,".png");
-    iftImage *orig = iftReadImageByExt(fs->files[i]->path);
-    /* normalize  image */
-    iftImage *norm = iftNormalize(orig,0,255);
-    /* binarize image */
-    iftImage *aux1 = iftBelowAdaptiveThreshold(norm, NULL, A, 0.98, 2, 255);
-    /* remove noise components from the background */
-    iftImage *aux2 = iftSelectCompAboveArea(aux1,B,100);
-    iftDestroyImage(&aux1);
-    /* apply morphological filtering to make the fingerprint the
-       largest component: this operation must add frame and remove it
-       afterwards. */
-    aux1           = MyAsfCOBin(aux2,15.0);//iftAsfCOBin(aux2,15.0);
-    iftDestroyImage(&aux2);
-    /* close holes inside the components to allow subsequent erosion
-       from the external borders only */
-    aux2           = MyCloseBasins(aux1); //iftCloseBasins(aux1,NULL,NULL);
-    iftDestroyImage(&aux1);
-    /* erode components and select the largest one to estimate its
-       center as close as possible to the center of the fingerprint */    
-    iftSet *S = NULL;
-    aux1           = MyErodeBin(aux2,&S,30.0);//iftErodeBin(aux2,&S,30.0); 
-    iftDestroySet(&S);
-    iftDestroyImage(&aux2);
-    aux2           = iftSelectLargestComp(aux1,B);
+   iftFileSet *fs   = iftLoadFileSetFromDirBySuffix(argv[1],".png", 1);
+   int nimages      = fs->n;
+   char *out_dir    = argv[2];
+   iftMakeDir(out_dir);
+   iftAdjRel *A     = iftCircular(3.5), *B = iftCircular(1.5);
+   for (int i=0; i < nimages; i++) {
+      iftSet *S = NULL;
+      char *basename = iftFilename(fs->files[i]->path,".png");
+      iftImage *orig = iftReadImageByExt(fs->files[i]->path);
+      /* normalize  image */
+      iftImage *norm = iftNormalize(orig,0,255);
+      /* binarize image */
+      iftImage *aux1 = iftBelowAdaptiveThreshold(norm, NULL, A, 0.98, 2, 255);
+      /* remove noise components from the background */
+      iftImage *aux2 = iftSelectCompAboveArea(aux1,B,100);
+      iftDestroyImage(&aux1);
+      printf("[2] Set size => %d\n", iftSetSize(S));
+      aux1 = MyDilateBin(aux2, &S, 15.0);
+      printf("[3] Set size => %d\n", iftSetSize(S));
 
-    /* crop the normalized image by the minimum bounding box of the
-       resulting mask (largest component) */ 
+      /* apply morphological filtering to make the fingerprint the
+      largest component: this operation must add frame and remove it
+      afterwards.
+      */
+      //  aux1           = MyAsfCOBin(aux2,15.0);//iftAsfCOBin(aux2,15.0);
+      //  iftDestroyImage(&aux2);
+      //  /* close holes inside the components to allow subsequent erosion
+      //     from the external borders only */
+      //  aux2           = MyCloseBasins(aux1); //iftCloseBasins(aux1,NULL,NULL);
+      //  iftDestroyImage(&aux1);
+      //  /* erode components and select the largest one to estimate its
+      //     center as close as possible to the center of the fingerprint */
+      //  iftSet *S = NULL;
+      //  aux1           = MyErodeBin(aux2,&S,30.0);//iftErodeBin(aux2,&S,30.0);
+      //  iftDestroySet(&S);
+      //  iftDestroyImage(&aux2);
+      //  aux2           = iftSelectLargestComp(aux1,B);
 
-    iftDestroyImage(&aux1);
-    iftVoxel pos;
-    iftBoundingBox bb = iftMinBoundingBox(aux2, &pos);    
-    aux1              = iftExtractROI(norm,bb); 
-      
-    sprintf(filename,"%s/%s.png",out_dir,basename);
-    iftWriteImageByExt(aux1,filename);
-    iftDestroyImage(&aux1);
-    iftDestroyImage(&aux2);
-    iftDestroyImage(&orig);
-    iftDestroyImage(&norm);
-    iftFree(basename);
-  }
+      //  /* crop the normalized image by the minimum bounding box of the
+      //     resulting mask (largest component) */
+
+      //  iftDestroyImage(&aux1);
+      //  iftVoxel pos;
+      //  iftBoundingBox bb = iftMinBoundingBox(aux2, &pos);
+      //  aux1              = iftExtractROI(norm,bb);
+
+      sprintf(filename,"%s/%s.png",out_dir,basename);
+      //  iftWriteImageByExt(aux1,filename);
+      iftDestroyImage(&aux1);
+      iftDestroyImage(&aux2);
+      iftDestroyImage(&orig);
+      iftDestroyImage(&norm);
+      iftDestroySet(&S);
+      iftFree(basename);
+      printf("Processed %d/%d images\n",i+1,nimages);
+   }
 
   iftDestroyFileSet(&fs);
   iftDestroyAdjRel(&A);
   iftDestroyAdjRel(&B);
-  
+
   puts("\nDone...");
   puts(iftFormattedTime(iftCompTime(tstart, iftToc())));
-  
+
   /* ---------------------------------------------------------- */
 
   info = mallinfo();
   MemDinFinal = info.uordblks;
   if (MemDinInicial!=MemDinFinal)
     printf("\n\nDinamic memory was not completely deallocated (%d, %d)\n",
-	   MemDinInicial,MemDinFinal);   
+	   MemDinInicial,MemDinFinal);
 
   return 0;
 }
