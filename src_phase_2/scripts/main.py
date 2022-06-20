5a0c92ac610374273b7828c92ea5d8beea6594f2
mateus oliveira <mateus.oliveira@icomp.ufam.edu.br>
2022-06-20 15:34:28 -0300

    Add on flow transformers for images.
    - Add flow VIT Transformer.
    - Add config yaml
    -

--- src_phase_2/scripts/main.py
+++ src_phase_2/scripts/main.py
@@ -3,7 +3,7 @@ from torch_snippets import DataLoader, optim
 import torch
 from data_loaders import BasicTransformations, BasicDataset
 from losses import ContrastiveLoss
-from models import SimpleConvSiameseNN, PreTrainedVGGSiameseNN
+from models import SimpleConvSiameseNN, PreTrainedVGGSiameseNN, ViTSiamese
 from torchsummary import summary
 from tqdm import trange
 import gc
@@ -13,23 +13,25 @@ import argparse
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 FACTORY_DICT = {
-    "model" : {
-        "SimpleConvSiameseNN" : SimpleConvSiameseNN,
-        "PreTrainedVGGSiameseNN" : PreTrainedVGGSiameseNN,
+    "model": {
+        "SimpleConvSiameseNN": SimpleConvSiameseNN,
+        "PreTrainedVGGSiameseNN": PreTrainedVGGSiameseNN,
+        "ViTSiamese": ViTSiamese
     },
-    "dataset" : {
-        "BasicDataset" : BasicDataset
+    "dataset": {
+        "BasicDataset": BasicDataset
     },
-    "transformation" : {
-        "BasicTransformations" : BasicTransformations
+    "transformation": {
+        "BasicTransformations": BasicTransformations
     },
-    "optimizer" : {
-        "Adam" : optim.Adam
+    "optimizer": {
+        "Adam": optim.Adam
     },
-    "loss" : {
-        "ContrastiveLoss" : ContrastiveLoss
+    "loss": {
+        "ContrastiveLoss": ContrastiveLoss
     }
 }
+
 
 def get_dataset(dataset_configs, configs):
     transform_type = list(dataset_configs.values())[0]["transform"]
@@ -43,8 +45,9 @@ def get_dataset(dataset_configs, configs):
     dataset = FACTORY_DICT["dataset"][list(dataset_configs.keys())[0]](
         **dataset_configs[list(dataset_configs.keys())[0]]
     )
-    
+
     return dataset
+
 
 def experiment_factory(configs):
     train_dataset_configs = configs["train_dataset"]
@@ -52,7 +55,7 @@ def experiment_factory(configs):
     model_configs = configs["model"]
     optimizer_configs = configs["optimizer"]
     criterion_configs = configs["loss"]
-    
+
     # Construct the dataloaders with any given transformations (if any)
     train_dataset = get_dataset(train_dataset_configs, configs)
     validation_dataset = get_dataset(validation_dataset_configs, configs)
@@ -75,11 +78,13 @@ def experiment_factory(configs):
 
     return model, train_loader, validation_loader, optimizer, criterion
 
+
 def parse_yaml_file(yaml_path):
     with open(yaml_path, "r") as yaml_file:
         configurations = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
     return configurations
+
 
 def run_train_epoch(model, optimizer, criterion, loader, monitoring_metrics, epoch):
     model.train()
@@ -96,7 +101,7 @@ def run_train_epoch(model, optimizer, criterion, loader, monitoring_metrics, epo
             optimizer.step()
 
             running_loss += loss.cpu()
-            running_accuracy  += accuracy.cpu()
+            running_accuracy += accuracy.cpu()
 
             progress_bar.set_postfix(
                 desc=f"[Epoch {epoch}] Loss: {running_loss:.3e} - Acc {running_accuracy:.3e}"
@@ -108,6 +113,7 @@ def run_train_epoch(model, optimizer, criterion, loader, monitoring_metrics, epo
     monitoring_metrics["accuracy"]["train"].append(
         (running_accuracy / len(loader)).detach().numpy()
     )
+
 
 def run_validation(model, criterion, loader, monitoring_metrics, epoch):
     with torch.no_grad():
@@ -125,7 +131,7 @@ def run_validation(model, criterion, loader, monitoring_metrics, epoch):
                 loss, accuracy = criterion(features_1, features_2, labels)
 
                 running_loss += loss.cpu()
-                running_accuracy  += accuracy.cpu()
+                running_accuracy += accuracy.cpu()
 
                 progress_bar.set_postfix(
                     desc=f"[Epoch {epoch}] Loss: {running_loss:.3e} - Acc {running_accuracy:.3e}"
@@ -138,12 +144,13 @@ def run_validation(model, criterion, loader, monitoring_metrics, epoch):
             running_accuracy / len(loader)
         )
 
-def run_training_experiment(model, train_loader, validation_loader, optimizer, 
-        criterion, configs
-    ):
+
+def run_training_experiment(model, train_loader, validation_loader, optimizer,
+                            criterion, configs
+                            ):
     monitoring_metrics = {
-        "loss" : {"train" : [], "validation" : []},
-        "accuracy" : {"train" : [], "validation" : []}
+        "loss": {"train": [], "validation": []},
+        "accuracy": {"train": [], "validation": []}
     }
 
     for epoch in range(1, configs["epochs"] + 1):
@@ -157,6 +164,7 @@ def run_training_experiment(model, train_loader, validation_loader, optimizer,
     export_learning_curves(monitoring_metrics, output_folder=configs["path_to_save_report"])
     torch.save(model, configs["path_to_save_model"])
 
+
 if __name__ == "__main__":
     # Reads YAML that sets configurations for training experiment
     parser = argparse.ArgumentParser(description="Fingerprint Identification Model Training Framework")
@@ -165,12 +173,12 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     configurations = parse_yaml_file(args.config_file)
-    
+
     model, train_loader, validation_loader, optimizer, criterion = experiment_factory(configurations)
 
     summary(model)
 
     run_training_experiment(
-        model, train_loader, validation_loader, optimizer, 
+        model, train_loader, validation_loader, optimizer,
         criterion, configurations
     )
--- src_phase_2/scripts/models.py
+++ src_phase_2/scripts/models.py
@@ -1,50 +1,91 @@
 from torch_snippets import nn
+from vit_transformer import PatchEmbedding, TransformerEncoder
+
+class ViT(nn.Sequential):
+    def __init__(self,
+                 in_channels: int,
+                 patch_size: int,
+                 emb_size: int,
+                 img_size: int,
+                 depth: int,
+                 **kwargs):
+        super().__init__(
+            PatchEmbedding(in_channels, patch_size, emb_size, img_size),
+            TransformerEncoder(depth, emb_size=emb_size, **kwargs),
+        )
+
+
+class ViTSiamese(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 patch_size: int,
+                 emb_size: int,
+                 img_size: int,
+                 depth: int,
+                 **kwargs):
+        super(ViTSiamese, self).__init__()
+
+        self.vit_transformer = ViT(in_channels,
+                                   patch_size,
+                                   emb_size,
+                                   img_size,
+                                   depth,
+                                   **kwargs)
+
+    def forward(self, input_1, input_2):
+        output_1 = self.vit_transformer(input_1)
+        output_2 = self.vit_transformer(input_2)
+
+        return output_1, output_2
+
 
 def ConvBlock(channels_in, channels_out, kernel_size=3):
     return nn.Sequential(
-        nn.Conv2d(channels_in, channels_out, 
-                    kernel_size=kernel_size, padding=kernel_size//2,
-                    bias=False
-                ),
-            nn.BatchNorm2d(channels_out),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=kernel_size, stride=2, padding=1
-        )
+        nn.Conv2d(channels_in, channels_out,
+                  kernel_size=kernel_size, padding=kernel_size // 2,
+                  bias=False
+                  ),
+        nn.BatchNorm2d(channels_out),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=kernel_size, stride=2, padding=1
+                     )
     )
+
 
 class SimpleConvSiameseNN(nn.Module):
     def __init__(self, input_size):
         super(SimpleConvSiameseNN, self).__init__()
         self.input_size = input_size
-        
+
         self.features = nn.Sequential(
             ConvBlock(channels_in=1, channels_out=128),
             ConvBlock(channels_in=128, channels_out=64),
             ConvBlock(channels_in=64, channels_out=32),
             nn.Flatten(),
-            nn.Linear(32*(self.input_size[0]//8)*(self.input_size[1]//8), 512), 
+            nn.Linear(32 * (self.input_size[0] // 8) * (self.input_size[1] // 8), 512),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 256), 
+            nn.Linear(512, 256),
             nn.ReLU(inplace=True),
             nn.Linear(256, 64)
         )
-    
+
     def forward(self, input_1, input_2):
         output_1 = self.features(input_1)
         output_2 = self.features(input_2)
 
         return output_1, output_2
 
+
 class PreTrainedVGGSiameseNN(nn.Module):
     def __init__(self):
         super(PreTrainedVGGSiameseNN, self).__init__()
-        
+
         """
         [TODO] 
         - DEFINE VGG MODEL WITH PRETRAINED WEIGHTS
         - ADD A HEAD TO EXTRACT THE FEATURES (TRAINABLE PARAMETERS)
         """
-    
+
     def forward(self, input_1, input_2):
         output_1 = self.features(input_1)
         output_2 = self.features(input_2)
