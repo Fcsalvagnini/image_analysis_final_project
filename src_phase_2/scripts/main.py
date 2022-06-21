07bbcb425fce203e0e080885280d00aa4015023c
Fcsalvagnini <felip.eng@outlook.com.br>
2022-06-21 02:01:14 +0000

    Fix printing of running loss and accuracy

--- src_phase_2/scripts/main.py
+++ src_phase_2/scripts/main.py
@@ -91,13 +91,14 @@ def parse_yaml_file(yaml_path):
     return configurations
 
 
-def run_train_epoch(model, optimizer, criterion, loader, monitoring_metrics, epoch):
+def run_train_epoch(model, optimizer, criterion, loader, 
+        monitoring_metrics, epoch, batch_size):
     model.train()
     model.to(DEVICE)
     running_loss = 0
     running_accuracy = 0
     with trange(len(loader), desc='Train Loop') as progress_bar:
-        for _, batch in zip(progress_bar, loader):
+        for batch_idx, batch in zip(progress_bar, loader):
             image_1, image_2, labels = [data.to(DEVICE) for data in batch]
             optimizer.zero_grad()
             features_1, features_2 = model(image_1, image_2)
@@ -109,7 +110,7 @@ def run_train_epoch(model, optimizer, criterion, loader, monitoring_metrics, epo
             running_accuracy += accuracy.cpu()
 
             progress_bar.set_postfix(
-                desc=f"[Epoch {epoch}] Loss: {running_loss:.3e} - Acc {running_accuracy:.3e}"
+                desc=f"[Epoch {epoch}] Loss: {running_loss/(batch_idx * batch_size):.3e} - Acc {running_accuracy/(batch_idx * batch_size):.3e}"
             )
 
     monitoring_metrics["loss"]["train"].append(
@@ -120,7 +121,8 @@ def run_train_epoch(model, optimizer, criterion, loader, monitoring_metrics, epo
     )
 
 
-def run_validation(model, criterion, loader, monitoring_metrics, epoch):
+def run_validation(model, criterion, loader, monitoring_metrics, 
+        epoch, batch_size):
     with torch.no_grad():
         torch.cuda.empty_cache()
         gc.collect()
@@ -130,7 +132,7 @@ def run_validation(model, criterion, loader, monitoring_metrics, epoch):
         running_loss = 0
         running_accuracy = 0
         with trange(len(loader), desc='Validation Loop') as progress_bar:
-            for _, batch in zip(progress_bar, loader):
+            for batch_idx, batch in zip(progress_bar, loader):
                 image_1, image_2, labels = [data.to(DEVICE) for data in batch]
                 features_1, features_2 = model(image_1, image_2)
                 loss, accuracy = criterion(features_1, features_2, labels)
@@ -139,7 +141,7 @@ def run_validation(model, criterion, loader, monitoring_metrics, epoch):
                 running_accuracy += accuracy.cpu()
 
                 progress_bar.set_postfix(
-                    desc=f"[Epoch {epoch}] Loss: {running_loss:.3e} - Acc {running_accuracy:.3e}"
+                    desc=f"[Epoch {epoch}] Loss: {running_loss/(batch_idx * batch_size):.3e} - Acc {running_accuracy/(batch_idx * batch_size):.3e}"
                 )
 
         monitoring_metrics["loss"]["validation"].append(
@@ -160,10 +162,12 @@ def run_training_experiment(model, train_loader, validation_loader, optimizer,
 
     for epoch in range(1, configs["epochs"] + 1):
         run_train_epoch(
-            model, optimizer, criterion, train_loader, monitoring_metrics, epoch
+            model, optimizer, criterion, train_loader, monitoring_metrics,
+            epoch,  batch_size=configs["batch_size"] 
         )
         run_validation(
-            model, criterion, validation_loader, monitoring_metrics, epoch
+            model, criterion, validation_loader, monitoring_metrics,
+            epoch, batch_size=configs["batch_size"]
         )
 
     export_learning_curves(monitoring_metrics, output_folder=configs["path_to_save_report"])
