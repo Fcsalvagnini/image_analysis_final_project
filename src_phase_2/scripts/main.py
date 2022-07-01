912a1b2dc348859fb94dd730128d46c21a37f8c3
Mateus Oliveira da Silva <mateus.osilva@neobpo.com.br>
2022-07-01 14:09:07 -0300

    machine learning models

--- src_phase_2/scripts/main.py
+++ src_phase_2/scripts/main.py
@@ -2,9 +2,9 @@ from utils import export_learning_curves, config_flatten
 from torch_snippets import DataLoader, optim
 import torch
 from data_loaders import BasicDataset, BasicStratifiedDataset, \
-                        BasicDatasetTriplet, BasicTransformations, \
-                        DatasetRawTraining
-
+    BasicDatasetTriplet, BasicTransformations, \
+    DatasetRawTraining
+from utils import create_pairs_balanced
 from save_best_model import SaveBestModel
 from losses import ContrastiveLoss, TripletLoss
 from models import SimpleConvSiameseNN, PreTrainedVGGSiameseNN, ViTSiamese, ViTSiameseTriplet
@@ -112,7 +112,7 @@ def run_train_epoch(model, optimizer, criterion, loader,
     with trange(len(loader), desc='Train Loop') as progress_bar:
         for batch_idx, batch in zip(progress_bar, loader):
             # if        
-            image_1, image_2, labels = [data.to(DEVICE) for data in batch]
+            image_1, image_2, labels, _, _ = [data.to(DEVICE) for data in batch]
             optimizer.zero_grad()
             features_1, features_2 = model(image_1, image_2)
             loss, accuracy = criterion(features_1, features_2, labels)
@@ -139,6 +139,8 @@ def run_train_epoch(model, optimizer, criterion, loader,
 
 def run_validation(model, optimizer, criterion, loader, monitoring_metrics,
                    epoch, batch_size):
+    valid_dataset = []
+
     with torch.no_grad():
         torch.cuda.empty_cache()
         gc.collect()
@@ -149,7 +151,7 @@ def run_validation(model, optimizer, criterion, loader, monitoring_metrics,
         running_accuracy = 0
         with trange(len(loader), desc='Validation Loop') as progress_bar:
             for batch_idx, batch in zip(progress_bar, loader):
-                image_1, image_2, labels = [data.to(DEVICE) for data in batch]
+                image_1, image_2, labels, name_1, name_2 = [data.to(DEVICE) for data in batch]
                 features_1, features_2 = model(image_1, image_2)
                 loss, accuracy = criterion(features_1, features_2, labels)
 
@@ -159,18 +161,64 @@ def run_validation(model, optimizer, criterion, loader, monitoring_metrics,
                 progress_bar.set_postfix(
                     desc=f"[Epoch {epoch}] Loss: {running_loss / (batch_idx + 1):.3f} - Acc {running_accuracy / (batch_idx + 1):.3f}"
                 )
+                valid_dataset.append((name_1, features_1, name_2, features_2, labels))
 
+        print(valid_dataset)
         accuracy_valid = (running_accuracy / (batch_idx + 1))
-        save_best_model(accuracy_valid,
-                        epoch,
-                        model,
-                        optimizer,
-                        criterion)
+        # save_best_model(accuracy_valid,
+        #                 epoch,
+        #                 model,
+        #                 optimizer,
+        #                 criterion)
 
     epoch_loss = (running_loss / len(loader)).detach().numpy()
     epoch_acc = (running_accuracy / len(loader)).detach().numpy()
     monitoring_metrics["loss"]["validation"].append(epoch_loss)
     monitoring_metrics["accuracy"]["validation"].append(epoch_acc)
+
+    return epoch_loss, epoch_acc
+
+
+def run_validation_ml(model, optimizer, criterion, loader, monitoring_metrics,
+                      epoch, batch_size):
+    valid_dataset = []
+
+    with torch.no_grad():
+        torch.cuda.empty_cache()
+        gc.collect()
+    #
+    #     model.to(DEVICE)
+        model.eval()
+        running_loss = 0
+        running_accuracy = 0
+        list_validation = create_pairs_balanced(compare_file)
+        for
+    #     with trange(len(loader), desc='Validation Loop') as progress_bar:
+    #         for batch_idx, batch in zip(progress_bar, loader):
+    #             image_1, image_2, labels = [data.to(DEVICE) for data in batch]
+    #             features_1, features_2 = model(image_1, image_2)
+    #             loss, accuracy = criterion(features_1, features_2, labels)
+    #
+    #             running_loss += loss.cpu()
+    #             running_accuracy += accuracy.cpu()
+    #
+    #             progress_bar.set_postfix(
+    #                 desc=f"[Epoch {epoch}] Loss: {running_loss / (batch_idx + 1):.3f} - Acc {running_accuracy / (batch_idx + 1):.3f}"
+    #             )
+    #             valid_dataset.append((name_1, features_1, name_2, features_2, labels))
+    #
+    #     print(valid_dataset)
+    #     accuracy_valid = (running_accuracy / (batch_idx + 1))
+        # save_best_model(accuracy_valid,
+        #                 epoch,
+        #                 model,
+        #                 optimizer,
+        #                 criterion)
+
+    # epoch_loss = (running_loss / len(loader)).detach().numpy()
+    # epoch_acc = (running_accuracy / len(loader)).detach().numpy()
+    # monitoring_metrics["loss"]["validation"].append(epoch_loss)
+    # monitoring_metrics["accuracy"]["validation"].append(epoch_acc)
 
     return epoch_loss, epoch_acc
 
@@ -189,7 +237,7 @@ def run_training_experiment(model, train_loader, validation_loader, optimizer,
             model, optimizer, criterion, train_loader, monitoring_metrics,
             epoch, batch_size=configs["batch_size"]
         )
-        valid_loss, valid_acc = run_validation(
+        valid_loss, valid_acc = run_validation_ml(
             model, optimizer, criterion, validation_loader, monitoring_metrics,
             epoch, batch_size=configs["batch_size"]
         )
@@ -198,7 +246,6 @@ def run_training_experiment(model, train_loader, validation_loader, optimizer,
                        'valid_acc': valid_acc, 'valid_loss': valid_loss})
 
     export_learning_curves(monitoring_metrics, output_folder=configs["path_to_save_report"])
-    torch.save(model, configs["path_to_save_model"])
 
 
 if __name__ == "__main__":
@@ -212,7 +259,6 @@ if __name__ == "__main__":
 
     model, train_loader, validation_loader, optimizer, criterion = experiment_factory(configurations)
 
-    # summary(model)
     fconfigurations = {}
     fconfigurations = config_flatten(configurations, fconfigurations)
     if configurations['wandb']:
