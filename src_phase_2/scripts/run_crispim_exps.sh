f0e69f445fdb08016251e09e5e2657d5abf678f9
Fcsalvagnini <felip.eng@outlook.com.br>
2022-07-11 21:04:48 +0000

    Add configuration files and bash script to run batch of exps

---
+++ src_phase_2/scripts/run_crispim_exps.sh
@@ -0,0 +1,12 @@
+python main.py configs/crispim_configs_to_report/ConvFingerprintSiamese_140.yml &&
+python main.py configs/crispim_configs_to_report/ConvFingerprintSiamese_300.yml &&
+python main.py configs/crispim_configs_to_report/SCConvFingerprintSiameseV1_140_pr2.yml &&
+python main.py configs/crispim_configs_to_report/SCConvFingerprintSiameseV1_140_pr4.yml &&
+python main.py configs/crispim_configs_to_report/SCConvFingerprintSiameseV1_300_pr2.yml &&
+python main.py configs/crispim_configs_to_report/SCConvFingerprintSiameseV1_300_pr4.yml &&
+python main.py configs/crispim_configs_to_report/SCConvFingerprintSiameseV2_140_pr2.yml &&
+python main.py configs/crispim_configs_to_report/SCConvFingerprintSiameseV2_140_pr4.yml &&
+python main.py configs/crispim_configs_to_report/SCConvFingerprintSiameseV2_300_pr2.yml &&
+python main.py configs/crispim_configs_to_report/SCConvFingerprintSiameseV2_300_pr4.yml &&
+python main.py configs/crispim_configs_to_report/SimpleConvNet_140.yml &&
+python main.py configs/crispim_configs_to_report/SimpleConvNet_300.yml
